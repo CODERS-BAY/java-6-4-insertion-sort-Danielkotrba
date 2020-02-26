c280411d5cd6d7e729409ebb3437595beb09e3e6
Daniel <Daniel.kotrba@gmx.at>
2020-02-26 09:33:57 +0100

    Insertion Sort. not 100% sure why it works, but it does

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+    int[] numbers = {7, 87, 9, 51, -8, 64};
+        System.out.println(Arrays.toString(numbers));
+
+        for (int i = 1; i < numbers.length;i++){
+            int unsorted = numbers[i];
+            int temp = i - 1;
+
+            while (temp >= 0 && numbers[temp] > unsorted) {
+                numbers[temp + 1] = numbers[temp];
+                temp = temp - 1;
+            }
+            numbers[temp + 1] = unsorted;
+            System.out.println(Arrays.toString(numbers));
+        }
+
+    }
+}
